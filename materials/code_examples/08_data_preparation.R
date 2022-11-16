b9f6626a2588d965880c92756eababee8db7c216
Ulrich Matter <ulrich.matter@unibas.ch>
2022-11-16 08:56:51 +0100

    update materials week 8

---
+++ materials/code_examples/08_data_preparation.R
@@ -0,0 +1,156 @@
+
+# SET UP --------------
+
+# load packages
+library(tidyverse)
+
+
+
+## Data types, strings ------------------------------------------------------------------------
+messy_df <- data.frame(last_name = c("Wayne", "Trump", "Karl Marx"),
+                       first_name = c("John", "Melania", ""),
+                       gender = c("male", "female", "Man"),
+                       date = c("2018-11-15", "2018.11.01", "2018/11/02"),
+                       income = c("150,000", "250000", "10000"), stringsAsFactors = FALSE)
+
+
+
+
+
+
+
+
+## define, clean categorical variables ------------------------------------------------------------------------
+messy_df$gender <- as.factor(messy_df$gender)
+messy_df$gender
+# Problems?
+
+
+
+
+## standardize categories ------------------------------------------------------------------------
+messy_df$gender[messy_df$gender == "Man"] <- "male"
+messy_df$gender
+
+
+## tidyverse approach
+messy_df$gender <- fct_recode(messy_df$gender, "male" = "Man")
+messy_df$gender
+
+
+
+
+
+
+
+
+## numeric/integer values ------------------------------------------------------------------------
+as.integer(messy_df$income)
+# Problem?
+
+
+## ------------------------------------------------------------------------
+messy_df$income <- str_replace(messy_df$income, pattern = ",", replacement = "")
+messy_df$income <- as.integer(messy_df$income)
+
+
+
+## clean up variables  ------------------------------------------------------------------------
+messy_df
+# remaining problems?
+
+
+
+
+
+## split strings, add new variables
+splitnames <- str_split(messy_df$last_name, pattern = " ", simplify = TRUE)
+splitnames
+
+## find rows with problematic cases, add correct first name, 
+problem_cases <- messy_df$first_name == ""
+messy_df$first_name[problem_cases] <- splitnames[problem_cases, 1]
+
+
+#last name
+messy_df$last_name[problem_cases] <- splitnames[problem_cases, 2]
+messy_df
+
+
+##  parse dates correctly -------------------------------------------------------
+library(lubridate)
+
+
+## ------------------------------------------------------------------------
+messy_df$date <- ymd(messy_df$date)
+
+
+## ------------------------------------------------------------------------
+messy_df
+
+
+## ------------------------------------------------------------------------
+str(messy_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## ----echo=FALSE, warning=FALSE, message=FALSE----------------------------
+tidydata <- gather(data = rawdata, treatmenta, treatmentb, key = "treatment", value = "result" )
+tidydata$treatment <- gsub("treatment", "", tidydata$treatment)
+
+
+
+
+## ------------------------------------------------------------------------
+wide_df <- data.frame(last_name = c("Wayne", "Trump", "Marx"),
+                       first_name = c("John", "Melania", "Karl"),
+                       gender = c("male", "female", "male"),
+                       income.2018 = c("150000", "250000", "10000"),
+                      income.2017 = c( "140000", "230000", "15000"),stringsAsFactors = FALSE)
+wide_df
+
+
+## ------------------------------------------------------------------------
+long_df <- gather(wide_df, income.2018, income.2017, key = "year", value = "income")
+long_df
+
+
+## ------------------------------------------------------------------------
+long_df$year <- str_replace(long_df$year, "income.", "")
+long_df
+
+
+## ------------------------------------------------------------------------
+weird_df <- data.frame(last_name = c("Wayne", "Trump", "Marx", "Wayne", "Trump", "Marx", 
+                                        "Wayne", "Trump", "Marx"),
+                       first_name = c("John", "Melania", "Karl", "John", "Melania", "Karl", 
+                                        "John", "Melania", "Karl"),
+                       gender = c("male", "female", "male", "male", "female", "male", 
+                                        "male", "female", "male"),
+                       value = c("150000", "250000", "10000", "2000000", "5000000", "NA", 
+                                        "50", "25", "NA"),
+                       variable = c( "income", "income", "income", "assets", "assets", "assets", 
+                                        "age", "age", "age"), stringsAsFactors = FALSE)
+weird_df
+
+
+## ------------------------------------------------------------------------
+tidy_df <- spread(weird_df, key = "variable", value = "value")
+tidy_df
+
